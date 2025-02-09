f41f28e45c28e94bcab0d42e505ee34c90c42897
enp6s0 <enp6s0@users.noreply.github.com>
2025-02-08 20:52:09 -0800

    MVP: level and mute states can now be changed through API

--- dspgate.py
+++ dspgate.py
@@ -11,7 +11,7 @@
 from dsp.Tesira import *
 from transports.SSH import *
 import time, os, yaml, logging
-from flask import Flask, jsonify, request 
+from flask import Flask, request, jsonify
 from functools import wraps
 
 # Version
@@ -101,6 +101,75 @@ def getBlockInfo(blockID: str):
         return jsonify({"error": f"DSP block {blockID} not found"}), 404
 
     return jsonify(block), 200
+
+# Set attribute of a specific block
+@dspMustBeReady
+@app.route("/block/<string:blockID>", methods = ["POST", "PUT", "PATCH"])
+def setBlockAttribute(blockID: str):
+    """
+    Set attribute on a specific block
+    """
+    reqContent = request.get_json(force = True, silent = False)
+    if not reqContent:
+        return jsonify({"error": f"Empty request"}), 400
+
+    # Block must exist
+    block = dsp.block(blockID)
+    if not block:
+        return jsonify({"error": f"DSP block {blockID} not found"}), 404
+
+    # What kind of block are we dealing with?
+    # Mute / level?
+    if block["type"] in ["MuteControl", "LevelControl"]:
+
+        # Channel settings are required
+        if "channel" not in reqContent:
+            return jsonify({"error": f"Channel must be specified"}), 412
+
+        # Channel must be specified as a key-value dict
+        if type(reqContent["channel"]) != dict:
+            return jsonify({"error": f"Invalid channel specification type"}), 412
+
+        # For each channel, we process change requests
+        for channel, changeRequest in reqContent["channel"].items():
+
+            try:
+                channel = int(channel)
+            except ValueError:
+                return jsonify({"error": f"Value change on {blockID}, non-numeric channel received: {channel}"}), 412
+
+            # Change requests are also expected to be key-value dicts
+            if type(changeRequest) != dict:
+                return jsonify({"error": f"Invalid change request type on channel {channel}"}), 412
+
+            # For each change request, we have key (what to change) and value (what to set it to)
+            for changeKey, changeValue in changeRequest.items():
+
+                # Mute state change?
+                if changeKey in ["mute", "muted"]:
+                    dsp.setMute(blockID, channel, value = True if str(changeValue).strip().lower() in ["true", "yes", "mute", "muted"] else False)
+
+                elif changeKey in ["level"]:
+                    # Level change, only valid on level control blocks
+                    if block["type"] in ["LevelControl"]:
+
+                        # We must be able to convert change value into float
+                        try:
+                            changeValue = float(changeValue)
+                        except ValueError:
+                            return jsonify({"error": f"Value change on {blockID} channel {channel}, non-numeric value received"}), 412
+
+                        dsp.setLevel(blockID, channel, value = changeValue)
+
+                    else:
+                        return jsonify({"error": f"Level adjustment on unsupported block type {block['type']}"}), 412
+
+                else:
+                    # Unknown. We log this and just move on
+                    logger.warning(f"Unknown change key: {changeKey} (on {blockID})")
+
+        # Once done, we return update OK
+        return jsonify({"update": "ok"}), 200
 
 # Let's get the show rolling!
 if __name__ == "__main__":
--- readme.md
+++ readme.md
@@ -8,3 +8,21 @@ A lightweight gateway to enable HTTP-based control of Biamp Tesira DSPs (through
 
 * DSP attributes listing
 * `Mute` and `Level` DSP blocks
+
+### Usage examples
+
+#### Get all supported blocks: send `GET` to `/block`
+
+#### Get values: send `GET` to `/block/<blockID>`
+
+#### Set values: send `POST` (or `PATCH`) to `/block/<blockID>` with JSON body:
+
+Mute (or unmute) all channels (special channel `0` = all channels):
+```
+{"channel": {"0" : {"mute": "true"}}}
+```
+
+Set level of channels:
+```
+{"channel": {"0" : {"level": "-10"}}}
+```
